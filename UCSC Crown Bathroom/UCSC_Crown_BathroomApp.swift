84c4d8efbbca9ced1eae0928c35fe124332c7ddd
Jonathan Morris <jonathan_wellington@icloud.com>
2021-09-03 16:32:31 -0700

    Initial Commit

---
+++ UCSC Crown Bathroom/UCSC_Crown_BathroomApp.swift
@@ -0,0 +1,17 @@
+//
+//  UCSC_Crown_BathroomApp.swift
+//  UCSC Crown Bathroom
+//
+//  Created by Jonathan Morris on 9/3/21.
+//
+
+import SwiftUI
+
+@main
+struct UCSC_Crown_BathroomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
